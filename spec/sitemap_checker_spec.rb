b182cf1d910b1af8ab8e58204e64e694fe09d1ec
GerlandoP <gerlando@gmail.com>
2012-07-30 20:46:50 -0700

    added siteindex support

--- spec/sitemap_checker_spec.rb
+++ spec/sitemap_checker_spec.rb
@@ -8,16 +8,24 @@ describe SitemapChecker do
     @dir  = Pathname.new(File.dirname(__FILE__))
     stub_request(:any, "http://www.github.com").to_return(:status => 200, :body => 'foo')
     stub_request(:any, "http://www.github.com/404").to_return(:status => 404, :body => 'foo')
-    stub_request(:any, "http://www.github.com/index.xml").to_return(:status => 200, :body => File.read(@dir + 'fixtures/valid_sitemap.xml'))
-    stub_request(:any, "http://www.github.com/index.xml.gz").to_return(:status => 200, :body => File.read(@dir + 'fixtures/valid_sitemap.xml.gz'))
-    stub_request(:get, "http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd").
-      with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => File.read(@dir + 'fixtures/sitemap_schema.xml'), :headers => {})
+    stub_request(:any, "http://www.github.com/sitemap.xml").to_return(:status => 200, :body => File.read(@dir + 'fixtures/sitemap.xml'))
+    stub_request(:any, "http://www.github.com/sitemap.xml.gz").to_return(:status => 200, :body => File.read(@dir + 'fixtures/sitemap.xml.gz'))
+    stub_request(:any, "http://www.github.com/siteindex.xml").to_return(:status => 200, :body => File.read(@dir + 'fixtures/siteindex.xml'))
+    stub_request(:any, "http://www.github.com/siteindex.xml.gz").to_return(:status => 200, :body => File.read(@dir + 'fixtures/siteindex.xml.gz'))
+    stub_request(:get, "http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd").to_return(:status => 200, :body => File.read(@dir + 'fixtures/sitemap.xsd'), :headers => {})
+    stub_request(:get, "http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd").to_return(:status => 200, :body => File.read(@dir + 'fixtures/siteindex.xsd'), :headers => {})
   end
 
-  it "accepts xml and gzipped files" do
-    @xml_sitemap = SitemapChecker::Checker.new('http://www.github.com/index.xml')
-    @gz_sitemap = SitemapChecker::Checker.new('http://www.github.com/index.xml.gz')
+  it "accepts xml and gzipped siteindexes" do
+    @xml_sitemap = SitemapChecker::Checker.new('http://www.github.com/siteindex.xml')
+    @gz_sitemap = SitemapChecker::Checker.new('http://www.github.com/siteindex.xml.gz')
+    @xml_sitemap.status_list.size.should eq(4)
+    @gz_sitemap.status_list.size.should eq(4)
+  end
+
+  it "accepts xml and gzipped sitemaps" do
+    @xml_sitemap = SitemapChecker::Checker.new('http://www.github.com/sitemap.xml')
+    @gz_sitemap = SitemapChecker::Checker.new('http://www.github.com/sitemap.xml.gz')
     @xml_sitemap.status_list.size.should eq(2)
     @gz_sitemap.status_list.size.should eq(2)
   end
@@ -26,9 +34,14 @@ describe SitemapChecker do
     lambda {SitemapChecker::Checker.new('http://www.github.com')}.should raise_error(RuntimeError, 'Invalid Schema')
   end
 
-  it "returns list of urls with responses" do
-    @valid_sitemap = SitemapChecker::Checker.new('http://www.github.com/index.xml')
-    @valid_sitemap.status_list.should eq([['http://www.github.com','200'], ['http://www.github.com/404','404']])
+  it "returns list of urls with responses from sitemap" do
+    @sitemap = SitemapChecker::Checker.new('http://www.github.com/sitemap.xml')
+    @sitemap.status_list.should eq([['http://www.github.com','200'], ['http://www.github.com/404','404']])
+  end
+
+  it "returns list of urls with responses from siteindex" do
+    @siteindex = SitemapChecker::Checker.new('http://www.github.com/siteindex.xml')
+    @siteindex.status_list.should eq([['http://www.github.com','200'], ['http://www.github.com/404','404'], ['http://www.github.com','200'], ['http://www.github.com/404','404']])
   end
 
 end
